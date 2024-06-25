e8ebfa614086214d0e3f670d1ed048f09eeac279
S Kasish <98868800+SKasishDunu@users.noreply.github.com>
2024-06-25 16:38:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const menuBtn = document.getElementById("menu-btn");
+const navlinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+
+    navlinks.classList.toggle("open");
+
+    const isopen = navlinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
+});
+
+navlinks.addEventListener("click", (e) => {
+
+    navlinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line")
+});
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h2", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
